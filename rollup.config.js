3471365fa857dcb4c19fcf6d5cc528964cff44a2
Tom Kristian Tjemsland <tom.tjemsland@bouvet.no>
2019-10-29 12:34:53 +0100

    Added docs

--- rollup.config.js
+++ rollup.config.js
@@ -2,28 +2,47 @@ import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: 'dist/bundle.cjs.js',
-      format: 'cjs',
-    },
-    {
-      file: 'dist/bundle.esm.js',
-      format: 'esm',
-    },
-    {
+export default [
+  { // CJS and ESM
+    input: 'src/index.js',
+    output: [
+      {
+        file: 'dist/bundle.cjs.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/bundle.esm.js',
+        format: 'esm',
+      },
+    ],
+    plugins: [
+      resolve(),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      terser({
+        output: {
+          comments: 'all',
+        },
+        mangle: false,
+      }),
+    ],
+  },
+  { // UMD
+    input: 'src/index.js',
+    output: {
       name: 'videxMath',
       file: 'dist/bundle.umd.js',
       format: 'umd',
     },
-  ],
-  plugins: [
-    resolve(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    terser(),
-  ],
-};
+    plugins: [
+      resolve(),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      terser({
+        mangle: false,
+      }),
+    ],
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
-export default function floor(val) {
-  return Math.floor(val);
+import { RAD2DEG, DEG2RAD, TAU } from './const';
+
+/**
+ * Clamps the value to min or max if value is less than min or greater than max.
+ * @param {number} value Value to clamp
+ * @param {number} [min] Minimum value (Default: 0)
+ * @param {number} [max] Maximum value (Default: 1)
+ * @return {number} Clamped value
+ */
+export function clamp(value, min, max) {
+  if (min === undefined) min = 0;
+  if (max === undefined) max = 1;
+  if (value < min) return min;
+  if (value > max) return max;
+  return value;
+}
+
+/**
+ * Implementation of glsl smoothstep function.
+ * @param {number} a Edge0
+ * @param {number} b Edge1
+ * @param {number} x threshold
+ */
+export function smoothstep(a, b, x) {
+  const t = clamp((x - a) / (b - a));
+  return t * t * (3.0 - 2.0 * t);
+}
+
+/**
+ * Linear interpolation between two numbers.
+ * @param {number} a Number to interpolate from
+ * @param {number} b Number to interpolate to
+ * @param {number} t Interpolation parameter, 0 = a and 1 = b
+ * @return {number} The interpolated value
+ */
+export function lerp(a, b, t) {
+  const m = clamp(t, 0, 1);
+  return a * (1 - m) + b * m;
+}
+
+/**
+ * Rounds a number to the specific number of digits.
+ * @param {number} value Value to round
+ * @param {number} [digits] Number of digits (Default: 1)
+ * @return {number} Rounded value
+ */
+export function round(value, digits) {
+  if (digits === undefined) digits = 1;
+  const f = 10 ** digits;
+  return Math.round(value * f) / f;
+}
+
+/**
+ * Convert degrees to radians.
+ * @param {number} deg Angle in degrees
+ * @returns {number} Angle in radians
+ */
+export function radians(deg) {
+  return deg * DEG2RAD;
+}
+
+/**
+ * Convert radians to degrees.
+ * @param {number} rad Angle in radians
+ * @returns {number} Angle in degrees
+ */
+export function degrees(rad) {
+  return rad * RAD2DEG;
+}
+
+/**
+ * Normalise an angle to be between -PI to +PI.
+ * @param {number} r Angle in radians
+ * @return {number} Normalised angle
+ */
+export function nrad(r) {
+  const v = r % TAU;
+  return (v < 0 ? v + TAU : v);
 }
